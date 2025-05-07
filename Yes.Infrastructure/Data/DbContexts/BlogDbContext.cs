c9ce25d92be6ed119968d4675207ed9c39e796a2
xiang <27800734@qq.com>
2025-05-07 22:54:38 +0800

    1

--- Yes.Infrastructure/Data/DbContexts/BlogDbContext.cs
+++ Yes.Infrastructure/Data/DbContexts/BlogDbContext.cs
@@ -21,7 +21,7 @@
             _ = _databaseType switch
             {
                 DatabaseTypeEnum.SqlServer => builder.UseSqlServer(_connectionString),
-                DatabaseTypeEnum.MySql => builder.UseMySql(_connectionString, string.IsNullOrEmpty(_databaseVersion) ? MySqlServerVersion.AutoDetect(_connectionString) : new MySqlServerVersion(_databaseVersion)),
+                DatabaseTypeEnum.MySql => builder.UseMySql(_connectionString, string.IsNullOrEmpty(_databaseVersion) ? MySqlServerVersion.AutoDetect(_connectionString) : new MySqlServerVersion(_databaseVersion), builder => builder.TranslateParameterizedCollectionsToConstants()),
                 DatabaseTypeEnum.PostgreSQL => builder.UseNpgsql(_connectionString),
 
                 _ => throw new DatabaseNotSupportedException()
